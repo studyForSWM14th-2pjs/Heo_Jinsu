952544dcc4dd850ec1a9c79e6e50d5b5ff7bf3c7
HJTN <zintnm21@gmail.com>
2023-01-09 14:30:32 +0900

    백준 12865 진행 중

--- Week1/BJ12865.py
+++ Week1/BJ12865.py
@@ -5,40 +5,37 @@ def find_value(weight, value, idx):
     # print(f'\nCurrent Weight: {weight}')
     # print(f'Current Value: {value}')
     # print(f'Current Index: {idx}')
-
+    
     if weight <= K and value > max_value:
         # print('Max Value Changed!')
         max_value = value
 
-    if weight > K or idx >= N:
-        return 0
-    else:
+    if checking(weight, idx):
         find_value(weight + weights[idx], value + values[idx], idx + 1)
         find_value(weight, value, idx + 1)
+
+def checking(weight, idx):
+    global N, K, weights
+    
+    if idx >= N:
+        return False
+        
+    for i in range(N):
+        if i != idx-1 and  weights[i] <= K - weight:
+            return True
+    
+    return False
 
 # N: The number of items / K: Maximnum weight
 N, K = map(int,input().split())
 
-weights = list()
-values = list()
-# W: Weight of the item / V: Value of the item
-for _ in range(N):
+dp = [[0]*(K+1) for _ in range(N+1)]
+
+for idx in range(1, N+1):
     W, V = map(int,input().split())
+    for i in range(1, K+1):
+        dp[idx][i] = max(dp[idx][i], dp[idx-1][i], dp[idx][i-1])
+        if i >= W:
+            dp[idx][i] = max(dp[idx][i], dp[idx-1][i-W] + V)
 
-    weights.append(W)
-    values.append(V)
-
-max_value = 0
-
-for idx in range(N):
-    weight = 0
-    value = 0
-
-    # print(f'\nCurrent Index: {idx}')
-    
-    # print(j, weight, value)
-
-    if value > max_value:
-        max_value = value
-
-print(max_value)
+print(dp[N][K])
